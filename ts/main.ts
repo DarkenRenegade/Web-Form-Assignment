b4c43c7a49ef3862b8731496c9433e83bfb7eb4d
Clay Wright <gclaywright@gmail.com>
2024-05-02 18:34:39 -0700

    Here is my TypeScript file.

---
+++ ts/main.ts
@@ -0,0 +1,21 @@
+/**
+ * This code represents a single snack that can be purchased.
+ */
+class Snack {
+    name : string;
+
+    description : string;
+
+    price : number;
+
+    expirationDate : Date;
+}
+
+// Snack object test code
+// let mySnack = new Snack();
+// mySnack.name = "Lay's Chips";
+// mySnack.description = "Crunchy potato chips";
+// mySnack.price = 19.99;
+// mySnack.expirationDate = new Date(2024, 10, 8);
+
+// console.log(mySnack);
